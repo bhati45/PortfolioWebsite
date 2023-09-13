7631b6977c94c41adfbb0afa3ab98a9101f1b897
bhati45 <bhati.4@iitj.ac.in>
2023-09-14 02:20:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+            window.scrollTo({
+                top: targetElement.offsetTop - 50, // Adjust the offset if needed
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Form validation
+document.getElementById('contact-form').addEventListener('submit', function (e) {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    if (!name || !email || !message) {
+        e.preventDefault();
+        alert('Please fill in all fields before submitting the form.');
+    }
+});
